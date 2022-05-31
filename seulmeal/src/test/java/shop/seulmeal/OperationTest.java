6d05a8f94b86c2b953cfaa31539c12e1f8e7cf71
jeongmoon <ijhghji@gmail.com>
2022-05-31 20:40:41 +0900

    ORACLE Wallet

--- seulmeal/src/test/java/shop/seulmeal/OperationTest.java
+++ seulmeal/src/test/java/shop/seulmeal/OperationTest.java
@@ -46,8 +46,8 @@ class OperationTest {
 		
 		Post post = new Post();
 		post.setUser(user);
-		post.setTitle("°øÁö»çÇ×");
-		post.setContent("°øÁö»çÇ× ³»¿ë");
+		post.setTitle("ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½");
+		post.setContent("ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½");
 		post.setPostStatus("1");
 		
 		// C
@@ -57,7 +57,7 @@ class OperationTest {
 		//post = operationMapper.getOperation(21);
 		
 		// U
-		post.setContent("¼öÁ¤µÈ °øÁö»çÇ×");
+		post.setContent("ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½");
 		post.setPostNo(21);
 		//operationMapper.updateOperation(post);
 		
@@ -79,9 +79,9 @@ class OperationTest {
 		int rs = operationMapper.getTotalCount(map);
 		
 		for (Post post2 : list) {
-			System.out.println("°øÁö : "+post2);
+			System.out.println("ï¿½ï¿½ï¿½ï¿½ : "+post2);
 		}
-		System.out.println("ÃÑ°ª : "+rs);
+		System.out.println("ï¿½Ñ°ï¿½ : "+rs);
 		
 		*/
 		
@@ -98,11 +98,11 @@ class OperationTest {
 		a4.setPostNo("21");
 		a5.setPostNo("21");
 		
-		a1.setAttachmentName("testÁß");
-		a2.setAttachmentName("testÁß");
-		a3.setAttachmentName("testÁß");
-		a4.setAttachmentName("testÁß");
-		a5.setAttachmentName("testÁß");
+		a1.setAttachmentName("testï¿½ï¿½");
+		a2.setAttachmentName("testï¿½ï¿½");
+		a3.setAttachmentName("testï¿½ï¿½");
+		a4.setAttachmentName("testï¿½ï¿½");
+		a5.setAttachmentName("testï¿½ï¿½");
 		
 		List<Attachments> list = new ArrayList<>();
 		list.add(a1);
@@ -114,14 +114,14 @@ class OperationTest {
 		*/
 		
 		Parts parts = new Parts();
-		parts.setName("±èÄ¡3");
+		parts.setName("ï¿½ï¿½Ä¡3");
 		parts.setCalorie(170);
 		parts.setPrice(1000);
 		
 		//productMapper.insertParts(parts);
 		
 		Map<String,Object> map = new HashMap<String,Object>();
-		map.put("name", "±èÄ¡2");
+		map.put("name", "ï¿½ï¿½Ä¡2");
 		
 		parts = productMapper.getParts(map);
 		
@@ -140,13 +140,13 @@ class OperationTest {
 		list = productMapper.getProductParts(1);
 		
 		for (Parts parts2 : list) {
-			System.out.println("1¹ø¿¡ µé¾î°£ Àç·á : "+parts2);
+			System.out.println("ìž¬ë£Œ : "+parts2);
 		}
 		/*
 		Comment cm = new Comment();
 		cm.setUser(user);
 		cm.setPostNo(10);
-		cm.setContent("¹®ÀÇ ´äº¯ÀÔ´Ï´Ù.");
+		cm.setContent("ï¿½ï¿½ï¿½ï¿½ ï¿½äº¯ï¿½Ô´Ï´ï¿½.");
 		int r =operationMapper.insertAnswer(cm);
 		System.out.println(r == 1);
 		*/
