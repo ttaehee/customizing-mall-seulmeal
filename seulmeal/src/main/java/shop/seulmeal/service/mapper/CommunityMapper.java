4d8c2a1c019d6db92704646c3647b6f599455235
ghm8614 <ghm8614@gmail.com>
2022-05-31 19:38:19 +0900

    Community : 2022.05.31 Create CommunityApplicationTests.java

--- seulmeal/src/main/java/shop/seulmeal/service/mapper/CommunityMapper.java
+++ seulmeal/src/main/java/shop/seulmeal/service/mapper/CommunityMapper.java
@@ -3,7 +3,6 @@ package shop.seulmeal.service.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 import shop.seulmeal.service.domain.Comment;
 import shop.seulmeal.service.domain.Post;
